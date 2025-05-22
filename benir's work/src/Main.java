257b7ece2e18e4e5b9a95af3e0cf31cb534f6911
benir-o <beniromenda@gmail.com>
2025-05-22 23:09:17 +0300

    Connection of User class to database

--- benir's work/src/Main.java
+++ benir's work/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Benir will be posting his work here");
+        User admin=new User("Kylian Mbappe","kmbappe@gmail.com","xz3v58");
+        userDatabase db=new userDatabase();
+        db.saveUser(admin);
     }
 }
