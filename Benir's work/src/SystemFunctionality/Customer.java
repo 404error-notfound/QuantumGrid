08b1ec40cd9f98d1c1d39751ec7ad8bbb78918b3
benir-o <beniromenda@gmail.com>
2025-06-09 12:59:29 +0300

    Work on Checktokens() Method

--- Benir's work/src/SystemFunctionality/Customer.java
+++ Benir's work/src/SystemFunctionality/Customer.java
@@ -1,5 +1,10 @@
 package SystemFunctionality;
 
+import PageUI.QuantPage;
+
+import javax.swing.*;
+import java.sql.*;
+
 public class Customer extends User {
 
     private Integer userNo;
@@ -15,6 +20,36 @@ public class Customer extends User {
     //Use a sql query to retrieve token amount (probably)
     @Override
     void checkTokens() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn= DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/user_db",
+                    "root",
+                    "ManCity@254"
+            );
+            int customer_id=this.UserId;
+            if (customer_id==this.UserId){
+                String sqlStatement="SELECT customer_tokens FROM customers ";
+                PreparedStatement stmt=conn.prepareStatement(sqlStatement);
+                ResultSet resultSet= stmt.executeQuery();
+                if (resultSet.next()){
+                    String customer_tokens=resultSet.getString("customer_tokens");
+                    JOptionPane.showMessageDialog(null,"You have "+customer_tokens+ " tokens");
+                }else{
+                    JOptionPane.showMessageDialog(null,"Invalid Email or Password input");
+                }
+                resultSet.close();
+                stmt.close();
+                conn.close();
+            }
+            else{
+                JOptionPane.showMessageDialog(null,"Enter correct customer ID");
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
 
     }
 
---
+++ Benir's work/src/SystemFunctionality/SystemTests.java
@@ -0,0 +1,9 @@
+package SystemFunctionality;
+
+public class SystemTests {
+    public static void main(String[] args) {
+        Customer c1= new Customer(1,"benir odeny","n@mail.com","abcdef",101,150.00);
+        c1.checkTokens();
+
+    }
+}
