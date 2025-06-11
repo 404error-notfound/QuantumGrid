4c91f4834d8713b2c653d027f5b63c6bc2c05747
benir-o <beniromenda@gmail.com>
2025-06-11 23:54:59 +0300

    CheckTokens Method bulk completion

--- Benir's work/src/SystemFunctionality/Customer.java
+++ Benir's work/src/SystemFunctionality/Customer.java
@@ -9,15 +9,15 @@ public class Customer extends User {
 
     private Integer userNo;
     private Double Tokens;
+    private String customer_password;
 
     public Customer(Integer customerId,String name, String email, String password, Integer userNo,Double Tokens) {
         super(customerId,name, email, password);
         this.userNo = userNo;
         this.Tokens=Tokens;
     }
-
-
-    //Use a sql query to retrieve token amount (probably)
+    //Use a sql query to retrieve token amount
+    //This should only happen when a user is logged in to the database or a user signs in to the database.
     @Override
     void checkTokens() {
         try {
@@ -27,30 +27,25 @@ public class Customer extends User {
                     "root",
                     "ManCity@254"
             );
-            int customer_id=this.UserId;
-            if (customer_id==this.UserId){
-                String sqlStatement="SELECT customer_tokens FROM customers ";
-                PreparedStatement stmt=conn.prepareStatement(sqlStatement);
-                ResultSet resultSet= stmt.executeQuery();
-                if (resultSet.next()){
-                    String customer_tokens=resultSet.getString("customer_tokens");
-                    JOptionPane.showMessageDialog(null,"You have "+customer_tokens+ " tokens");
-                }else{
-                    JOptionPane.showMessageDialog(null,"Invalid Email or Password input");
-                }
-                resultSet.close();
-                stmt.close();
-                conn.close();
+            Integer customerId=UserId;
+            String customerPassword=Password;
+            String sqlStatement="SELECT customer_tokens FROM customers c  JOIN users u on c.customer_id=u.id WHERE u.id=? AND u.password=?  ";
+            PreparedStatement stmt=conn.prepareStatement(sqlStatement);
+            stmt.setInt(1,customerId);
+            stmt.setString(2,customerPassword);
+            ResultSet resultSet= stmt.executeQuery();
+            if (resultSet.next()){
+                String customer_tokens=resultSet.getString("customer_tokens");
+                JOptionPane.showMessageDialog(null,"You have "+customer_tokens+ " tokens");
+            }else{
+                JOptionPane.showMessageDialog(null,"Invalid Email or Password input");
             }
-            else{
-                JOptionPane.showMessageDialog(null,"Enter correct customer ID");
-            }
+            resultSet.close();
+            stmt.close();
+            conn.close();
         } catch (ClassNotFoundException | SQLException e) {
             throw new RuntimeException(e);
         }
-
-
-
     }
 
     @Override
--- Benir's work/src/SystemFunctionality/SystemTests.java
+++ Benir's work/src/SystemFunctionality/SystemTests.java
@@ -4,6 +4,9 @@ public class SystemTests {
     public static void main(String[] args) {
         Customer c1= new Customer(1,"benir odeny","n@mail.com","abcdef",101,150.00);
         c1.checkTokens();
-
+        //Probably create a method that:
+        /*
+        Once logged in to a database, the customer is instantiated
+         */
     }
 }
